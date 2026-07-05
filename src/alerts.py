0cfe3bfae4d6f3bae8b1f78815aed0637ef96c0c
Royal <royalflemingdsouza@gmail.com>
2026-07-05 15:22:02 +0530

    Add alerting module

---
+++ src/alerts.py
@@ -0,0 +1,7 @@
+def show_alert(ip, attempts, risk):
+    print('\n' + '=' * 50)
+    print('SECURITY ALERT')
+    print(f'IP Address : {ip}')
+    print(f'Attempts   : {attempts}')
+    print(f'Risk Level : {risk}')
+    print('=' * 50)
